08b6c6295cb5baa0061741291979bac0ad90321b
ericpkatz <ericpkatz@gmail.com>
2021-02-13 11:54:00 -0500

    commit

---
+++ places.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    name: 'Wall Street',
+    location: [-74.0087719497395, 40.706214987392386],
+    isFavorite: true
+  },
+  {
+    name: 'Carwall Avenue',
+    location: [-73.82152799527827,40.92899626478586],
+    isFavorite: true
+  },
+  {
+    name: 'McKinney and Doyle',
+    location: [-73.60194783438757,41.56233616199611] 
+  },
+  {
+    name: 'Walnut Drive',
+    location: [-73.63098684765659, 41.62564823312732]
+  },
+  {
+    name: '382 CPW',
+    location: [-73.96559555310544, 40.79374204257131 ]
+  },
+  {
+    name: 'The Dakota',
+    location: [-73.97613648603168, 40.77675017163841 ]
+  },
+  {
+    name: 'Katz\'s Deli',
+    location: [-73.98739680699529,40.72243623785117]
+  },
+  {
+    name: 'Milbrook diner',
+    location: [-73.69530527801548, 41.78676179587256 ]
+  },
+  {
+    name: 'Galleria Mall',
+    location: [-73.9206301219385,41.62646469186266 ]
+  }
+];
---
+++ server.js
@@ -0,0 +1,85 @@
+try {
+  Object.entries(require('./secrets.js')).forEach(([key, value])=> {
+    process.env[key] = value;
+  });
+}
+catch(ex){
+  console.log(ex);
+}
+const Sequelize = require('sequelize');
+const { STRING, DECIMAL, BOOLEAN } = Sequelize;
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+
+const Place = conn.define('place', {
+  name: STRING,
+  lat: DECIMAL, 
+  lng: DECIMAL,
+  isFavorite: {
+    type: BOOLEAN,
+    defaultValue: false
+  } 
+});
+
+const syncAndSeed = async()=> {
+  await conn.sync({ force: true });
+  await Promise.all(require('./places').map(({ isFavorite, name, location: [ lng, lat] }) => {
+    return Place.create({
+      name,
+      lat,
+      lng,
+      isFavorite
+    });
+  }));
+};
+
+const path = require('path');
+const express = require('express');
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.json());
+
+app.get('/', (req, res, next)=> {
+  res.render(path.join(__dirname, 'index.html'), {
+    MAP_API_KEY: process.env.MAP_API_KEY,
+    PLACES_API_KEY: process.env.PLACES_API_KEY
+  });
+});
+
+
+app.get('/api/places', async(req, res, next)=> {
+  try {
+    res.send(await Place.findAll({ order: [['lat', 'desc']]}));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.put('/api/places/:id', async(req, res, next)=> {
+  try {
+    const place = await Place.findByPk(req.params.id);
+    await place.update(req.body);
+    res.send(place);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.post('/api/places/', async(req, res, next)=> {
+  try {
+    const place = await Place.create(req.body);
+    res.status(201).send(place);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+const init = ()=> {
+  syncAndSeed();
+  const port = process.env.PORT || 3000;
+  app.listen(port, ()=> console.log(`listening on port ${port}`));
+};
+
+init();
